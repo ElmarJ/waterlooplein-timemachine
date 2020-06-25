603fa9d8d3d8641386d1c7b828930fe61615b298
Elmar Jansen <elmar@elmarjansen.nl>
2020-06-25 11:34:27 +0200

    Add quality setting to settings menu for #18

---
+++ Assets/GameQualityMenuController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameQualityMenuController : MonoBehaviour
+{
+    private Dropdown dropdown;
+    void Start()
+    {
+        this.dropdown = gameObject.GetComponent<Dropdown>();
+        
+        // Populate dropdown:
+        var names = QualitySettings.names;
+
+        dropdown.options.Clear();
+        foreach (var name in names)
+        {
+            dropdown.options.Add(new Dropdown.OptionData(name));
+        }
+
+        // Set current value:
+        this.dropdown.SetValueWithoutNotify(QualitySettings.GetQualityLevel());
+    }
+
+    public void OnQualityChange()
+    {
+        QualitySettings.SetQualityLevel(dropdown.value, true);
+    }
+}
