1dc2b01740887850a0dd6060c1e5b5465ceb1acf
Elmar Jansen <elmar@elmarjansen.nl>
2020-08-05 14:52:14 +0200

    Deactivate UI screens when unavailable. Closes #29

--- source/Waterlooplein3D/Assets/Scripts/GameMenuController.cs
+++ source/Waterlooplein3D/Assets/Scripts/GameMenuController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameMenuController : MonoBehaviour
 {
+    private GameObject mainMenu;
+    private GameObject settingsMenu;
     private Canvas mainMenuCanvas;
     private Canvas settingsMenuCanvas;
     private FirstPersonAIO fpCharacter;
@@ -11,8 +13,10 @@ public class GameMenuController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.mainMenuCanvas = GameObject.Find("MainMenu").GetComponent<Canvas>();
-        this.settingsMenuCanvas = GameObject.Find("SettingsMenu").GetComponent<Canvas>();
+        this.mainMenu = GameObject.Find("MainMenu");
+        this.mainMenuCanvas = mainMenu?.GetComponent<Canvas>();
+        this.settingsMenu = GameObject.Find("SettingsMenu");
+        this.settingsMenuCanvas = this.settingsMenu?.GetComponent<Canvas>();
         this.fpCharacter = GameObject.Find("CharacterController").GetComponent<FirstPersonAIO>();
         
         this.mainMenuCanvas.enabled = true;
@@ -28,7 +32,11 @@ public class GameMenuController : MonoBehaviour
     public void ResumeGame()
     {
         this.mainMenuCanvas.enabled = false;
+        this.mainMenu.SetActive(false);
+
         this.settingsMenuCanvas.enabled = false;
+        this.settingsMenu.SetActive(false);
+
         this.fpCharacter.enableCameraMovement = true;
         this.fpCharacter.lockAndHideCursor = true;
         Cursor.lockState = CursorLockMode.Locked; Cursor.visible = false;
@@ -39,7 +47,11 @@ public class GameMenuController : MonoBehaviour
     public void ActivateMenu()
     {   
         this.mainMenuCanvas.enabled = true;
+        this.mainMenu.SetActive(true);
+
         this.settingsMenuCanvas.enabled = false;
+        this.settingsMenu.SetActive(false);
+
         this.fpCharacter.enableCameraMovement = false;
         this.fpCharacter.lockAndHideCursor = false;
         Cursor.lockState = CursorLockMode.None; Cursor.visible = true;
@@ -50,14 +62,19 @@ public class GameMenuController : MonoBehaviour
     public void ShowSettingsMenu()
     {
         this.mainMenuCanvas.enabled = false;
-        this.settingsMenuCanvas.enabled = true;
+        this.mainMenu.SetActive(false);
 
+        this.settingsMenuCanvas.enabled = true;
+        this.settingsMenu.SetActive(true);
     }
 
     public void ShowMainMenu()
     {
         this.mainMenuCanvas.enabled = true;
+        this.mainMenu.SetActive(true);
+
         this.settingsMenuCanvas.enabled = false;
+        this.settingsMenu.SetActive(false);
     }
 
     public void Exit()
