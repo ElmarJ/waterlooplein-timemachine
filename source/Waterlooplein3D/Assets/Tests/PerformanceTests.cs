11c025564fba55dd6a5f3aefc6eef605dc1fb81c
Elmar Jansen <elmar@elmarjansen.nl>
2021-01-11 00:47:45 +0100

    Add performance test (first attempt)

---
+++ source/Waterlooplein3D/Assets/Tests/PerformanceTests.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Collections;
+using NUnit.Framework;
+using Unity.PerformanceTesting;
+using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PerformanceTests
+    {
+        [UnityTest, Performance]
+        public IEnumerator BasicPerformanceTest()
+        {
+            // load scene
+            using (Measure.Scope("Scene Loading Time"))
+            {
+                SceneManager.LoadScene("Assets/Scenes/MainScene.unity");
+            }
+            
+            for (int i = 0; i < QualitySettings.names.GetLength(0); i++)
+            {
+                SceneManager.LoadScene("Assets/Scenes/MainScene.unity");
+                QualitySettings.SetQualityLevel(i, true);
+
+                Measure
+                    .Frames()
+                    .WarmupCount(10)
+                    .MeasurementCount(100)
+                    .SampleGroup("Frame execution time on quality:" + QualitySettings.names[i])
+                    .Run();
+            }
+
+            // Measure the execution time of all frames
+
+            using (Measure.ProfilerMarkers())
+            {
+                for (var i = 0; i < 60; i++)
+                {
+                    yield return null;
+                }
+            }
+
+            // measure memory
+            var allocated = new SampleGroup("TotalAllocatedMemory", SampleUnit.Megabyte);
+            var reserved = new SampleGroup("TotalReservedMemory", SampleUnit.Megabyte);
+            for (var i = 0; i < 60; i++)
+            {
+                Measure.Custom(allocated, Profiler.GetTotalAllocatedMemoryLong() / 1048576f);
+                Measure.Custom(reserved, Profiler.GetTotalReservedMemoryLong() / 1048576f);
+                yield return null;
+            }
+
+            yield return null;
+        }
+    }
+}
