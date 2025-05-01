facf9cdf4144ee37dc8b6507833e52eb89f2557c
Elmar Jansen <elmar@elmarjansen.nl>
2025-05-01 22:45:44 +0200

    Remove Framelimiter Awake function in runtime

--- source/WaterloopleinTimeMachine/Assets/Scripts/FrameRateLimiter.cs
+++ source/WaterloopleinTimeMachine/Assets/Scripts/FrameRateLimiter.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class FrameRateLimiter : MonoBehaviour
 {
+#if UNITY_EDITOR
     void Awake()
     {
-#if UNITY_EDITOR
         QualitySettings.vSyncCount = 0;  // VSync must be disabled
         Application.targetFrameRate = 60;
-#endif
     }
+#endif
 }
