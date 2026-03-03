feaa1b09c50c72d00be6c103c211a3be47719dcb
Elmar Jansen <elmar@elmarjansen.nl>
2026-03-03 16:10:07 +0100

    Solved several issues with menu navigation
    Had to refactor the game menu code into a seperate "game manager" (managing game state), a "menu controller" (managing menu state), and seperate controls for each menu (setting up data binding and events for each menu).

--- source/WaterloopleinTimeMachine/Assets/Scripts/GameMenuController.cs
+++ source/WaterloopleinTimeMachine/Assets/Scripts/GameMenuController.cs
@@ -6,136 +6,30 @@ using System.Linq;
 
 public class GameMenuController : MonoBehaviour
 {
-    public GameObject player;
-    public UIDocument mainMenu;
-    public UIDocument settingsMenu;
-    public UIDocument OSDControl;
-    public TimeController timeController;
-    private InputAction showMenuAction;
+    public GameObject mainMenu;
+    public GameObject settingsMenu; 
+    public GameObject OSDControl;
     private readonly string[] monthNames = { "Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December" };
 
-    public void Start()
-    {
-        this.showMenuAction = InputSystem.actions["ShowMenu"];
-        this.showMenuAction.Enable();
-        this.showMenuAction.performed += this.OnShowMenuClicked;
-    }
-
-    public void OnEnable()
-    {
-        this.ActivateMenu();
-        this.ShowMainMenu();
-    }
-
     // TODO: consider using a state machine that keeps track of the current menu (or general state of the game)
-    public void ActivateMenu()
-    {
-        this.SetMenuMouseState(true);
-        this.ShowMainMenu();
-    }
-
-    public void ResumeGame()
-    {
-        this.SetMenuMouseState(false);
-        this.HideAllMenus();
-    }
-
-    public void SetMenuMouseState(bool mouseHasMenuState)
-    {
-        UnityEngine.Cursor.lockState = mouseHasMenuState ? CursorLockMode.None : CursorLockMode.Locked;
-        UnityEngine.Cursor.visible = mouseHasMenuState;
-        if (mouseHasMenuState)
-        {
-            InputSystem.actions.FindActionMap("Player").Disable();
-            InputSystem.actions.FindActionMap("Game").Disable();
-            InputSystem.actions.FindActionMap("UI").Enable();
-        }
-        else
-        {
-            InputSystem.actions.FindActionMap("Player").Enable();
-            InputSystem.actions.FindActionMap("Game").Enable();
-            InputSystem.actions.FindActionMap("UI").Disable();
-        }
-        this.player.GetComponent<FirstPersonDrifter>().enabled = !mouseHasMenuState;
-    }
-
     public void ShowSettingsMenu()
     {
-        this.mainMenu.enabled = false;
-        this.settingsMenu.enabled = true;
-        this.OSDControl.enabled = false;
-
-        this.settingsMenu.rootVisualElement.Q<Button>("back-button").clicked += this.ShowMainMenu;
-
-        var qualityDropdown = this.settingsMenu.rootVisualElement.Q<DropdownField>("quality-dropdown");
-        qualityDropdown.choices = new(QualitySettings.names);
-        qualityDropdown.index = QualitySettings.GetQualityLevel();
-        qualityDropdown.RegisterValueChangedCallback(ev => QualitySettings.SetQualityLevel(qualityDropdown.index));
-
-        var resolutionDropdown = this.settingsMenu.rootVisualElement.Q<DropdownField>("resolution-dropdown");
-        resolutionDropdown.choices = (from resolution in Screen.resolutions select $"{resolution.width} x {resolution.height}").ToList();
-        resolutionDropdown.index = GetResolutionIndex();
-        resolutionDropdown.RegisterValueChangedCallback(ev => Screen.SetResolution(Screen.resolutions[resolutionDropdown.index].width, Screen.resolutions[resolutionDropdown.index].height, true));
+        this.mainMenu.SetActive(false);
+        this.settingsMenu.SetActive(true);
+        this.OSDControl.SetActive(false);
     }
 
     public void ShowMainMenu()
     {
-        this.mainMenu.enabled = true;
-        this.settingsMenu.enabled = false;
-        this.OSDControl.enabled = false;
-
-        this.mainMenu.rootVisualElement.Q<Button>("resume-button").clicked += this.ResumeGame;
-        this.mainMenu.rootVisualElement.Q<Button>("settings-button").clicked += this.ShowSettingsMenu;
-        this.mainMenu.rootVisualElement.Q<Button>("exit-button").clicked += this.ExitGame;
-
-        // Set the data context for the time controls to the Time Machine controller
-        this.mainMenu.rootVisualElement.Q<VisualElement>("time-controls").dataSource = this.timeController;
-        this.mainMenu.rootVisualElement.Q<Button>("resume-button").Focus();
+        this.mainMenu.SetActive(true);  
+        this.settingsMenu.SetActive(false);
+        this.OSDControl.SetActive(false);
     }
 
     public void HideAllMenus()
     {
-        this.mainMenu.enabled = false;
-        this.settingsMenu.enabled = false;
-        this.OSDControl.enabled = true;
+        this.mainMenu.SetActive(false);
+        this.settingsMenu.SetActive(false);
+        this.OSDControl.SetActive(true);
     }
-
-    public void ExitGame()
-    {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.ExitPlaymode();
-#else
-        Application.Quit();
-#endif
-    }
-
-    public void OnShowMenuClicked(InputAction.CallbackContext ctx)
-    {
-        if (ctx.performed)
-        {
-            this.ActivateMenu();
-        }
-    }
-
-    private int GetResolutionIndex()
-    {
-        // Find current resolution index.
-        // Note that Array.IndexOf with Screen.currentResolution does not work 
-        // because Resolution structs don't match (refresh rate data is missing).
-        int currentResolutionIndex = -1;
-        for (int i = 0; i < Screen.resolutions.Length; i++)
-        {
-            var resolution = Screen.resolutions[i];
-            if (resolution.height == Screen.height && resolution.width == Screen.width)
-            {
-                currentResolutionIndex = i;
-                break;
-            }
-        }
-
-        return currentResolutionIndex;
-    }
-
-
-
 }
--- source/WaterloopleinTimeMachine/Assets/Scripts/SettingsMenuController.cs
+++ source/WaterloopleinTimeMachine/Assets/Scripts/SettingsMenuController.cs
@@ -1,5 +1,61 @@
+using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class SettingsMenuController : MonoBehaviour
 {
+    private UIDocument uidoc;
+    public GameMenuController gameMenuController;
+
+    void OnEnable()
+    {
+        this.uidoc = GetComponent<UIDocument>();
+        this.uidoc.rootVisualElement.Q<Button>("back-button").clicked += gameMenuController.ShowMainMenu;
+
+        var cancelAction = InputSystem.actions["Cancel"];
+        cancelAction.Enable();
+        cancelAction.performed += ev => gameMenuController.ShowMainMenu();
+
+        var qualityDropdown = this.uidoc.rootVisualElement.Q<DropdownField>("quality-dropdown");
+        qualityDropdown.choices = new(QualitySettings.names);
+        qualityDropdown.index = QualitySettings.GetQualityLevel();
+        qualityDropdown.RegisterValueChangedCallback(ev => QualitySettings.SetQualityLevel(qualityDropdown.index));
+
+        var resolutionDropdown = this.uidoc.rootVisualElement.Q<DropdownField>("resolution-dropdown");
+        resolutionDropdown.choices = (from resolution in Screen.resolutions select $"{resolution.width} x {resolution.height}").ToList();
+        resolutionDropdown.index = GetResolutionIndex();
+        resolutionDropdown.RegisterValueChangedCallback(ev => Screen.SetResolution(Screen.resolutions[resolutionDropdown.index].width, Screen.resolutions[resolutionDropdown.index].height, true));
+    }
+
+    void OnDisable()
+    {
+        if (this.uidoc.rootVisualElement != null)
+        {
+            this.uidoc.rootVisualElement.Q<Button>("back-button").clicked -= gameMenuController.ShowMainMenu;
+        }
+    
+        var cancelAction = InputSystem.actions["Cancel"];
+        cancelAction.performed -= ev => gameMenuController.ShowMainMenu();
+        cancelAction.Disable();
+    }
+
+    private static int GetResolutionIndex()
+    {
+        // Find current resolution index.
+        // Note that Array.IndexOf with Screen.currentResolution does not work 
+        // because Resolution structs don't match (refresh rate data is missing).
+        int currentResolutionIndex = -1;
+        for (int i = 0; i < Screen.resolutions.Length; i++)
+        {
+            var resolution = Screen.resolutions[i];
+            if (resolution.height == Screen.height && resolution.width == Screen.width)
+            {
+                currentResolutionIndex = i;
+                break;
+            }
+        }
+
+        return currentResolutionIndex;
+    }
 }
